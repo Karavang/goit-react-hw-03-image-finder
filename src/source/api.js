21d482df92e36180f69367946f1dcfe4a982f1c5
Evgeny Shestak <107277013+Karavang@users.noreply.github.com>
2023-04-19 15:06:51 +0200

    for menthor

--- src/source/api.js
+++ src/source/api.js
@@ -2,6 +2,7 @@ const API_KEY = '32188250-5588add6a92db6c3bf4a2a30f';
 const URL = `https://pixabay.com/api/?key=${API_KEY}`;
 
 export function fetchSearch(searchText, per) {
+  console.log(per);
   return fetch(
     `${URL}&q=${searchText}&image_type=photo&orientation=horizontal&safesearch=true&per_page=${per}&page=1`
   ).then(res => res.json());
